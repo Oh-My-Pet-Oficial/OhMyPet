9e25c6046e8d3360d55d83af4cee888d195f8fde
Byorks <vanessabyorksp@gmail.com>
2024-11-05 16:35:50 -0300

    📱 feat: responsive header

--- script.js
+++ script.js
@@ -30,3 +30,48 @@ const reloadSlider = () => {
     let checkleft = itemsBanner[active].offsetLeft;
     listBanner.style.left = - checkleft + 'px';
 }
+
+// Header
+let menu = document.getElementById('menu');
+let iconBars = document.getElementById('icon-bars');
+let iconX = document.getElementById('icon-x');
+
+const openCloseMenu = () =>{
+    if ( menu.classList.contains("menu-closed") ) {
+        menu.classList.remove("menu-closed");
+
+        iconBars.style.display = "none";
+        
+        iconX.style.display = "inLine";  
+    }
+    else {
+        menu.classList.add("menu-closed");
+
+        iconX.style.display = "none";
+
+        iconBars.style.display = "inLine";
+    }
+}
+
+const menuResponsive = () => {
+    const mediaQuery = window.matchMedia('(max-width: 1350px)');
+    if (mediaQuery.matches) {
+        menu.classList.add("menu-closed");
+        iconX.style.display = "none";
+        iconBars.style.display = "inLine";
+    }else {
+        if(menu.classList.contains("menu-closed")){
+            menu.classList.remove("menu-closed");
+            iconX.style.display = "none";
+            iconBars.style.display = "inLine";
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    menuResponsive();
+});
+
+window.addEventListener('resize', () => {
+    menuResponsive();
+})
