e2334cab2044d8720a15deb0eddfa5345f0e9d23
Byorks <vanessabyorksp@gmail.com>
2024-11-06 16:23:15 -0300

    🚧 feat: web component header under development

--- script.js
+++ script.js
@@ -31,42 +31,42 @@ const reloadSlider = () => {
     listBanner.style.left = - checkleft + 'px';
 }
 
-// Header
-let menu = document.getElementById('menu');
-let iconBars = document.getElementById('icon-bars');
-let iconX = document.getElementById('icon-x');
+// // Header
+// let menu = document.getElementById('menu');
+// let iconBars = document.getElementById('icon-bars');
+// let iconX = document.getElementById('icon-x');
 
-const openCloseMenu = () =>{
-    if ( menu.classList.contains("menu-closed") ) {
-        menu.classList.remove("menu-closed");
+// const openCloseMenu = () =>{
+//     if ( menu.classList.contains("menu-closed") ) {
+//         menu.classList.remove("menu-closed");
 
-        iconBars.style.display = "none";
+//         iconBars.style.display = "none";
         
-        iconX.style.display = "inLine";  
-    }
-    else {
-        menu.classList.add("menu-closed");
+//         iconX.style.display = "inLine";  
+//     }
+//     else {
+//         menu.classList.add("menu-closed");
 
-        iconX.style.display = "none";
+//         iconX.style.display = "none";
 
-        iconBars.style.display = "inLine";
-    }
-}
+//         iconBars.style.display = "inLine";
+//     }
+// }
 
-const menuResponsive = () => {
-    const mediaQuery = window.matchMedia('(max-width: 1350px)');
-    if (mediaQuery.matches) {
-        menu.classList.add("menu-closed");
-        iconX.style.display = "none";
-        iconBars.style.display = "inLine";
-    }else {
-        if(menu.classList.contains("menu-closed")){
-            menu.classList.remove("menu-closed");
-            iconX.style.display = "none";
-            iconBars.style.display = "inLine";
-        }
-    }
-}
+// const menuResponsive = () => {
+//     const mediaQuery = window.matchMedia('(max-width: 1350px)');
+//     if (mediaQuery.matches) {
+//         menu.classList.add("menu-closed");
+//         iconX.style.display = "none";
+//         iconBars.style.display = "inLine";
+//     }else {
+//         if(menu.classList.contains("menu-closed")){
+//             menu.classList.remove("menu-closed");
+//             iconX.style.display = "none";
+//             iconBars.style.display = "inLine";
+//         }
+//     }
+// }
 
 document.addEventListener('DOMContentLoaded', () =>{
     menuResponsive();
